ab86d7db30021f6ff9dd6eec02c7f1fe65afb5ee
yeon3303 <yeon3303@gmail.com>
2021-04-19 16:07:18 +0900

    performance upgrade

--- main.js
+++ main.js
@@ -3,14 +3,20 @@ const lineY = document.querySelector('.lineY');
 const target = document.querySelector('.target');
 const xyCoord = document.querySelector('.xyCoord');
 
-document.addEventListener('mouseover', (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
-  lineX.style.top = `${y}px`;
-  lineY.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  xyCoord.style.top = `${y}px`;
-  xyCoord.style.left = `${x}px`;
-  xyCoord.innerHTML = `${x}px, ${y}px`
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+
+  document.addEventListener('mouseover', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    
+    lineX.style.transform = `translateY(${y}px)`;
+    lineY.style.transform = `translateX(${x}px)`;
+    target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+    xyCoord.style.transform = `translate(${x}px, ${y}px)`;
+    xyCoord.innerHTML = `${x}px, ${y}px`
+})  
 })
+
